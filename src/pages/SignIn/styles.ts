66326d7840950e016f18bb43548ce5581ec40cd7
joaohcca <joaohccalbu@gmail.com>
2021-04-06 21:08:52 -0300

    adding fonts and splash page

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components/native';
 
-const Container = styled.View``;
+export const Container = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
 
-export default Container;
+
+`;
+
+export const Title = styled.Text`
+    font-size: 20px;
+    color: #f4ede8;
+    font-family: 'RobotoSlab-Medium';
+`;
